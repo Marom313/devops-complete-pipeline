04afca2f1724b6d2f6ec266f508cce35bc237f14
Marom313 <marom313@gmail.com>
2026-02-10 10:56:21 +0200

    Phase 2: Fix Jenkinsfile

--- app/app.py
+++ app/app.py
@@ -7,7 +7,8 @@ def home():
     msg_html="""
     <div style="text-align:center; font-family:Arial; margin-top:100px;">
         <h1>Hello</h1>
-        <h2>This is Marom Gigi DevOps Home Assignment 🚀</h2>
+        <h2>This is Marom Gigi DevOps Home Assignment</h2> 
+        <h2>🚀</h2>
         <p>If you see this, the app is running and containerized :)</p>
     </div>
     """
